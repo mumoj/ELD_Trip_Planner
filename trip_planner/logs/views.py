dbe4668304e16b476fe82132afdd3b87351f023a
mumoj <mumojak@gmail.com>
2025-03-24 10:25:53 +0300

    Add CSRF

--- trip_planner/logs/views.py
+++ trip_planner/logs/views.py
@@ -1,13 +1,19 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from .models import DailyLog, LogEntry
 from .serializers import DailyLogSerializer, LogEntrySerializer
 from .log_generator import generate_log_image
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
+
+@method_decorator(csrf_exempt, name='dispatch')
 class DailyLogViewSet(viewsets.ModelViewSet):
     queryset = DailyLog.objects.all()
     serializer_class = DailyLogSerializer
+    permission_classes = [AllowAny]
     
     @action(detail=True, methods=['get'])
     def generate_image(self, request, pk=None):
@@ -24,7 +30,9 @@ class DailyLogViewSet(viewsets.ModelViewSet):
             'status': 'success',
             'image_url': daily_log.log_image.url
         })
-
+        
+@method_decorator(csrf_exempt, name='dispatch')
 class LogEntryViewSet(viewsets.ModelViewSet):
     queryset = LogEntry.objects.all()
     serializer_class = LogEntrySerializer
+    permission_classes = [AllowAny]
